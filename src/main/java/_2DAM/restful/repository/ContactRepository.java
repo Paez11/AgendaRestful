4197050f269e33eb7e9141948d61b83088370961
Paez11 <victorpaez117@gmail.com>
2023-01-13 10:41:16 +0100

    contact service

--- src/main/java/_2DAM/restful/repository/ContactRepository.java
+++ src/main/java/_2DAM/restful/repository/ContactRepository.java
@@ -1,4 +1,10 @@
 package _2DAM.restful.repository;
 
-public interface ContactRepository {
+import _2DAM.restful.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
 }
--- src/main/java/_2DAM/restful/services/ContactService.java
+++ src/main/java/_2DAM/restful/services/ContactService.java
@@ -1,4 +1,72 @@
 package _2DAM.restful.services;
 
+import _2DAM.restful.exceptions.RecordNotFoundException;
+import _2DAM.restful.model.Contact;
+import _2DAM.restful.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ContactService {
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    /**
+     * Get all contacts
+     * @return List of contacts
+     */
+    public List<Contact> getAllContacts() {
+        return contactRepository.findAll();
+    }
+
+    /**
+     * Get contact by id if exists or throw RecordNotFoundException
+     * @param id Contact id
+     * @return Contact
+     */
+    public Contact getContactById(Long id){
+        Optional<Contact> contact = contactRepository.findById(id);
+        if (contact.isPresent()){
+            return contact.get();
+        }else{
+            throw  new RecordNotFoundException("No contact record exist for given id", id);
+        }
+    }
+
+    /**
+     * Create a new contact or update if exists
+     * @param contact Contact
+     * @return Contact
+     */
+    public Contact createOrUpdateContact(Contact contact){
+        if (contact.getId()!=null){
+            Optional<Contact> c = contactRepository.findById(contact.getId());
+            if (c.isPresent()){
+                contact = contactRepository.save(contact);
+            }else{
+                throw new RecordNotFoundException("No contact record exist for given id", contact);
+            }
+        }else{
+            contact = contactRepository.save(contact);
+        }
+        return contact;
+    }
+
+    /**
+     * Delete contact by id if exists or throw RecordNotFoundException
+     * @param id Contact id
+     */
+    public void deleteContactById(Long id){
+        Optional<Contact> c = contactRepository.findById(id);
+        if (c.isPresent()){
+            contactRepository.deleteById(id);
+        }else{
+            throw new RecordNotFoundException("No contact record exist for given id", id);
+        }
+    }
+
 }
